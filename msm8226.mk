4a0aaaaf26d3b2e544e9b3c19cdf9b4784e91324
Rashmi Ramanna <rashmi@codeaurora.org>
2014-02-10 21:55:13 +0530

    Donot copy WCNSS_cfg.dat file from device target directory.
    As and when there is changes in configuration parameter's
    desctiption, the WCNSS_cfg.dat file in prima/firmware_bin
    directory is updated and copied to out directory for
    firmware binaries.
    So donot copy WCNSS_cfg.dat file from device target
    directory.
    
    Change-Id: If8824bcb49f85d30414ed927ffcd8657ef50ebeb
    CRs-Fixed: 609383

--- msm8226.mk
+++ msm8226.mk
@@ -48,7 +48,6 @@ PRODUCT_PACKAGES += init.qti.carrier.rc
 
 #wlan driver
 PRODUCT_COPY_FILES += \
-    device/qcom/msm8226/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     device/qcom/msm8226/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     device/qcom/msm8226/WCNSS_qcom_wlan_nv.bin:persist/WCNSS_qcom_wlan_nv.bin
 
